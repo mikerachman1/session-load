a71432b98f4ea143673c57b4550052de7b721d3c
Mike Rachman <mikerachman1@gmail.com>
2023-03-02 17:03:56 -0500

    added conditional rendering to total climb load

--- session-load/src/App.jsx
+++ session-load/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import findTops from './helpers/tops';
 import findLoad from './helpers/load';
+import findTotalLoad from './helpers/totalLoad';
 
 function App() {
   const [climbs, setClimbs] = useState([]); 
@@ -34,23 +35,32 @@ function App() {
       <div className='climbs-container'>
         {climbs.map((climb) => (
           <div key={climbs.indexOf(climb)} className='climb'>
-            <div className='climb-header'>
+            <div className='section'>
               <h2>{climb.name}</h2>
               <h2>V{climb.grade}</h2>
             </div>
-            <div className='climb-body'>
+            <div className='section'>
               <h3>Attempts: {climb.attempts.length}</h3>
               <h3>Tops: {findTops(climb.attempts)}</h3>
             </div>
-            <div className='climb-foot'>
+            <div className='section'>
               <h3>Attempt Load: 
                 {findLoad(climb.attempts, climb.grade).map((load, index) => (
                   <span key={index} className='load'>{load}</span>
                 ))}
               </h3>
             </div>
+            {climb.attempts.length > 1 &&
+              <div className='section'>
+                <h3>Total Climb Load: {findLoad(climb.attempts, climb.grade).reduce((a, c) => a + c)}</h3>
+              </div>
+            }
           </div>
         ))}
+      </div>
+      <div className='section'>
+        <h2>Total Session Load:</h2>
+        <h2>{findTotalLoad(climbs)}</h2>
       </div>
     </div>
   )
