d832101a3450bda444f238748ed28f38dc21593a
Mike Rachman <mikerachman1@gmail.com>
2023-03-07 09:56:07 -0500

    created Session component

---
+++ src/components/Session.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import '../styles/App.css';
+import '../styles/Sessions.css'
+
+function Session() {
+  
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Session
